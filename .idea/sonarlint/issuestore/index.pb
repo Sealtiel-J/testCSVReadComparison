25f0b46fe5374c84dd0568c106f03f9a42a72d4f
Sealtiel-J <jsgrosas@live.com>
2024-02-22 18:30:15 -0700

    commit with change to compareFromTxtFile

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+H
+testCSVReadComparison.py,a\e\ae268f893fb9606c39fa157e43f794e3dead014a
+C
+products_Coppel.txt,8\a\8ac99e763c043fa520dc3065c1d84bed583de0f3
+C
+EjemploCanonico.txt,0\1\018b7d5d5f2d2f33ea2657589e77e08e0a98f7d9
--- testCSVReadComparison.py
+++ testCSVReadComparison.py
@@ -1,6 +1,8 @@
+import json
 import os.path
 import logging
 import pandas as pd
+import json as js
 
 logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
@@ -41,14 +43,28 @@ def compareFromCsvFile(firstFile, secondFile):
 def compareFromTxtFile(firstFile, secondFile):
     logging.warning('Beginning compareFromTxtFile')
     try:
-        with open(firstFile, "r") as f:
-            textData = pd.read_json(f, lines=True, orient='records')
-        df = pd.DataFrame(textData)
-        firstJsonData = df[['id', 'sku']]
-        print(firstJsonData.nunique(0))
-        # textData.to_csv('compFile.csv',index=False)
-        # print(df.nunique(0))
 
+        with open(firstFile, mode="r", encoding='utf8', newline='\r\n') as f:
+            textData1 = pd.read_json(f, lines=True, orient='records')
+
+        with open(secondFile, mode="r", encoding='utf8', newline='\r\n') as f:
+            textData2 = pd.read_json(f, lines=True, orient='records')
+
+        df1 = pd.DataFrame(textData1[['sku', 'name']]).convert_dtypes()
+        df1['product_id'] = pd.to_numeric(df1['sku'])
+        print(type(df1['product_id']))
+        df2 = pd.DataFrame(textData2[['product_id', 'status']]).convert_dtypes()
+        #df2['product_id'] = pd.to_numeric(df2['product_id'],errors='coerse')
+        # firstJsonData = df1['sku', 'name']
+        # print(firstJsonData.nunique(0))
+        # secondJsonData = df2[['product_id', 'status']]
+        # print(secondJsonData.nunique(0))
+
+        #compResult = pd.merge(right=df1, left=df2,right_on='sku', left_on='product_id', how='inner')
+        compResult = pd.merge(df1, df2, on=['product_id'], how='left')
+        print(compResult)
+        compResult.to_csv('compFile.csv',index=False)
+        # print(df.nunique(0))
 
     except Exception as e:
         logging.error('Exeption occured', exc_info=True)
@@ -60,12 +76,13 @@ def main():
         # Define file path
         mainPath = "mainFile.csv"
         secondaryPath = "compFile.csv"
-        textFilePath = "EjemploCanonico.txt"
+        textMainPath = "ProductsCoppel.txt"
+        textSecondaryPath = "ProductBY.txt"
         # compareFromCsvFile(mainPath,secondaryPath)
-        compareFromTxtFile(textFilePath, secondaryPath)
+        compareFromTxtFile(textMainPath, textSecondaryPath)
 
     except Exception as e:
-        logging.error('Exeption occured', exc_info=True)
+        logging.error('Exeption occured ' + str(e), exc_info=True)
 
 
 if __name__ == '__main__':
